7ac9dd0d63ffd539feeeca463073100eb8105cdd
Erik Nordlund <contact@eriknordlund.com>
2018-12-04 19:23:42 -0800

    Added CameraMotionAccessory class from h-c-m

--- index.js
+++ index.js
@@ -65,3 +65,59 @@ ffmpegPlatform.prototype.didFinishLaunching = function() {
     self.api.publishCameraAccessories("Camera-ffmpeg", configuredAccessories);
   }
 }
+
+// An accessory with a MotionSensor service (from homebridge-camera-motion)
+class CameraMotionAccessory
+{
+  constructor(log, config, api) {
+    log(`CameraMotion accessory starting`);
+    this.log = log;
+    this.api = api;
+    config = config || {};
+    this.name = config.name_motion || 'Motion Detector';
+
+    this.pipePath = config.motion_pipe || '/tmp/motion-pipe';
+    this.timeout = config.motion_timeout !== undefined ? config.motion_timeout : 2000;
+
+    this.pipe = new FIFO(this.pipePath);
+    this.pipe.setReader(this.onPipeRead.bind(this));
+
+    this.motionService = new Service.MotionSensor(this.name);
+    this.setMotion(false);
+  }
+
+  setSource(cameraSource) {
+    this.cameraSource = cameraSource;
+  }
+
+  setMotion(detected) {
+    this.motionService
+      .getCharacteristic(Characteristic.MotionDetected)
+      .setValue(detected);
+  }
+
+  onPipeRead(text) {
+    console.log(`got from pipe: |${text}|`);
+    // on_picture_save printf '%f\t%n\t%v\t%i\t%J\t%K\t%L\t%N\t%D\n' > /tmp/camera-pipe
+    // http://htmlpreview.github.io/?https://github.com/Motion-Project/motion/blob/master/motion_guide.html#conversion_specifiers
+    // %f filename with full path
+    // %n number indicating filetype
+    // %v event
+    // %i width of motion area
+    // %J height of motion area
+    // %K X coordinates of motion center
+    // %L Y coordinates of motion center
+    // %N noise level
+    // %D changed pixels
+    const [filename, filetype, event, width, height, x, y, noise, dpixels] = text.trim().split('\t');
+    console.log('filename is',filename);
+    this.cameraSource.snapshot_path = filename;
+    this.setMotion(true);
+
+    setTimeout(() => this.setMotion(false), this.timeout); // TODO: is this how this works?
+  }
+
+  getServices() {
+    return [this.motionService];
+  }
+}
